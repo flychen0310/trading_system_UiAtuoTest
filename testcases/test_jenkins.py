1c5ecd74071ba74635affd90883f108649324914
chenhuazheng <1065249239@qq.com>
2023-03-19 15:35:34 +0800

    add allure

---
+++ testcases/test_jenkins.py
@@ -0,0 +1,8 @@
+#! C:\Users\AdiminiChen\AppData\Local\Programs\Python\Python310
+# coding=utf-8
+# @Time: 2023/3/19 15:34
+# @Author: jackchen
+class TestJenkins:
+
+    def test_jenkins(self, driver):
+        print("test_jenkins")
